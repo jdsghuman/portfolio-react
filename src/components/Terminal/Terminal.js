f143b9c66ec88f93234cd413cd5c384b664efdec
JD Ghuman <jdsghuman@gmail.com>
2019-06-16 22:16:18 -0500

    Edit terminal styling

--- src/components/Terminal/Terminal.js
+++ src/components/Terminal/Terminal.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import './Terminal.css';
 
 class Terminal extends Component {
+  state = {
+    clickedTerminal: false
+  }
   render() {
     return (
-      <>
+      <span onClick={this.clickTerminal}>
         {this.props.openTerminal && this.props.page === 'home' &&
-          <p className="terminal__blink terminal__blink-home">>></p>}
-
+          <p id="terminal__border" className={!this.state.clickedTerminal ? 'terminal__blink terminal__blink-home' : 'terminal__blink terminal__blink--clicked terminal__blink-home'}><span contentEditable="true" className="terminal__text--clicked"></span></p>}
+          {this.state.showTerminal && <input styles={{display: 'inline'}} type="text" />}
         {this.props.openTerminal && this.props.page === 'about' &&
-          <p className="terminal__blink terminal__blink-about">>></p>}
-      </>
+          <p id="terminal__border" className={!this.state.clickedTerminal ? 'terminal__blink terminal__blink-about' : 'terminal__blink terminal__blink--clicked terminal__blink-about'}><span contentEditable="true" className="terminal__text--clicked"></span></p>}
+      </span>
     )
+  }
+
+  clickTerminal = () => {
+    this.setState({ clickedTerminal: true });
   }
 }
 
