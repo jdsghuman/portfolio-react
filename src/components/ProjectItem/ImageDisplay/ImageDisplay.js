259bfb7ba8b78e953a5789fdd73688eb7164aee3
JD Ghuman <jdsghuman@gmail.com>
2019-08-04 20:20:10 -0500

    Clean up ImageDisplay component

--- src/components/ProjectItem/ImageDisplay/ImageDisplay.js
+++ src/components/ProjectItem/ImageDisplay/ImageDisplay.js
@@ -9,23 +9,14 @@ class ImageDisplay extends Component {
       this.setState({
         isLoaded: true
       })
-    }, 1000);
+    }, 800);
   }
 
-  renderSpinner = (image) => {
-      if(!this.state.isLoaded) {
-        return <p style={{ color: '#fff' }}>Is Loading...</p>
-      } else {
-        return <img className="project-item__img" alt="project" src={image} onLoad={this.imageLoaded} />
-      }
-  }
   render() {
-    let { thumbnail } = this.props;
-    let image = this.renderSpinner(thumbnail)
     return (
       <>
         {!this.state.isLoaded ? <div className="loader"></div> : null}
-        <img className={this.state.isLoaded ? 'project-item__img' : 'loader' } style={this.state.isLoaded ? {} : {display: 'none'}} alt="project" src={thumbnail} onLoad={this.imageLoaded} />
+        <img className={this.state.isLoaded ? 'project-item__img' : '' } style={this.state.isLoaded ? {} : {display: 'none'}} alt="project" src={this.props.thumbnail} onLoad={this.imageLoaded} />
       </>
     );
   }
