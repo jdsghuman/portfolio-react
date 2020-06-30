e8139f8993bb23bc4b45c93b39d98784e3aff341
JD Ghuman <jdsghuman@gmail.com>
2020-06-30 00:24:53 -0500

    Update redux devtools logger

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App.js';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import rootReducer from './redux/reducers/index';
 
-const store = createStore(
-    rootReducer,
-    applyMiddleware(logger),
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+  applyMiddleware(logger),
+));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>,
     document.getElementById('root'));
